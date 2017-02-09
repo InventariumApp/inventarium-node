21a018c32b0bddc5ec3b716968581a30c05b6103
Michael Rosenfield <mer95@case.edu>
2017-02-09 19:41:49 +0000

    Added add_items databse method

--- apiaiClient.js
+++ apiaiClient.js
@@ -1,6 +1,7 @@
 var config = require('./config');
 var twilio = require('./twilioClient');
 var phoneNumbers = require('./phone_numbers');
+var database = require('./database');
 
 module.exports.sendToChatbot = function(message, sessionId, phoneNumber, callback) {
     var apiai = require("apiai");
@@ -41,7 +42,7 @@ parseResponse = function(response, phoneNumber) {
         // if response_number.length < i {
         //     num = response_number[i];
         // }
-        addItem(response_items[i], num, phoneNumber);
+        database.addItem(response_items[i], num, phoneNumber);
     }
     // console.log(response)
     // console.log(items)
@@ -49,42 +50,3 @@ parseResponse = function(response, phoneNumber) {
     //console.log("response ***")
     return response_speech;
 };
-
-
-//**** FIREBASE STUFF **** <---- Will most likely move later...
-//TODO:  add user to the .indexOn in your security rules.
-var admin = require("firebase-admin");
-
-// Fetch the service account key JSON file contents
-var serviceAccount = require("./serviceAccountKey.json");
-
-// Initialize the app with a service account, granting admin privileges
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://inventarium-36e42.firebaseio.com"
-});
-
-// As an admin, the app has access to read and write all data, regardless of Security Rules
-var db = admin.database();
-var ref = db.ref("testingDB");
-
-addItem = function(item, number, phoneNumber) {
-    var listsRef = ref.child("lists")
-    listsRef.orderByChild("owner").equalTo(phoneNumber).on("child_added", function(snapshot) {
-        listID = snapshot.key;
-        var itemsRef = ref.child("lists/" + listID)
-        itemsRef.push({
-            item: item,
-            number: number
-        })
-    });
-}
-
-createList = function(phoneNumber) {
-    var listsRef = ref.child("lists")
-    listsRef.push({
-        owner: phoneNumber
-    });
-}
-
-//createList(phoneNumbers.israelPhoneNumber)
