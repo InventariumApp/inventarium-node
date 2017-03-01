33b5dd94b052ed89544d5ff0c062c930861522d5
Michael Rosenfield <mer95@case.edu>
2017-03-01 23:49:30 +0000

    querying database working

--- firebase.js
+++ firebase.js
@@ -1,7 +1,13 @@
 var admin = require("firebase-admin");
-var serviceAccount = require("root/firebase_server_key.json");
+var serviceAccount = require("/root/firebase_server_key.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://inventarium-36e42.firebaseio.com/"
+});
+
+var db = admin.database();
+var ref = db.ref("lists");
+ref.on("value", function(snapshot) {
+	console.log(snapshot.val());
 });
