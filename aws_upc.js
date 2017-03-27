67d5ae367f3f1aaaedf976d53043f6a88eca9e21
israelhill <idh@case.edu>
2017-03-27 20:11:12 +0000

    added repository to package.json. added test query to aws

--- aws_upc.js
+++ aws_upc.js
@@ -1,3 +1,12 @@
-let aws = require('aws-lib');
-let credentials = require('./aws_credentials');
-let prodAdv = aws.createProdAdvClient(credentials['accessKey'], credentials['secretKey'], yourAssociateTag);
+var aws = require('aws-lib');
+var credentials = require('./aws_credentials');
+var prodAdv = aws.createProdAdvClient(credentials['accessKey'], credentials['secretKey'], "inventarium39-20");
+
+var options = {SearchIndex: "Books", Keywords: "Javascript"}
+
+prodAdv.call("ItemSearch", options, function(err, result) {
+    console.log(result);
+    result['Items']['Item'].map(function(item) {
+        console.log(item);
+    });
+});
