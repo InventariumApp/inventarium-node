ae1a6949b57c14997fe7c9537d16df41b4cf6d6e
root <idh@case.edu>
2017-04-03 19:46:36 +0000

    added firebase helper functions to get shopping and pantry lists

--- firebase.js
+++ firebase.js
@@ -7,7 +7,51 @@ admin.initializeApp({
 });
 
 var db = admin.database();
-var ref = db.ref("lists");
-ref.on("value", function(snapshot) {
-	console.log(snapshot.val());
+var listRef = db.ref("lists");
+
+// ref.on("value", function(snapshot) {
+// 	console.log(snapshot.val());
+// });
+
+exports.getPantryListForUser = function(userEmail) {
+    admin.database().ref('lists/' + 'iphoneaccount@gmail,com').once('value').then(function(snapshot) {
+        var pantryList = snapshot.val()['pantry-list'];
+        var retVal = [];
+        for(item in pantryList) {
+            retVal.push(item);
+        }
+        console.log(retVal);
+        return retVal;
+    });
+}
+
+exports.getShoppingListForUser = function(userEmail) {
+    admin.database().ref('lists/' + 'iphoneaccount@gmail,com').once('value').then(function(snapshot) {
+        var shoppingList = snapshot.val()['shopping-list'];
+        var retVal = [];
+        for(item in shoppingList) {
+            retVal.push(item);
+        }
+        console.log(retVal);
+        return retVal;
+    });
+}
+
+// admin.database().ref('lists/' + 'iphoneaccount@gmail,com').once('value').then(function(snapshot) {
+//     var data = snapshot.val()['pantry-list'];
+//     console.log('Firebase data: \n\n');
+//     for(key in data) {
+//         console.log(key);
+//     }
+// });
+
+
+admin.database().ref('lists/' + 'iphoneaccount@gmail,com').once('value').then(function(snapshot) {
+    var shoppingList = snapshot.val()['shopping-list'];
+    var retVal = [];
+    for(key in shoppingList) {
+        retVal.push(key);
+    }
+    console.log(retVal);
+    return retVal;
 });
